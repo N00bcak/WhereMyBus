9cd67d4806193040409e21393455d62667c8982c
N00bcak <depictiger@gmail.com>
2023-03-06 22:21:10 +0800

    one fine day i will add favorite stations.

--- __main__.py
+++ __main__.py
@@ -10,19 +10,16 @@ class ArrivalCommandStates(StatesGroup):
     station = State()
     bus = State()
 
-# Handles command states for the /alertnextbus command.
-class AlertCommandStates(StatesGroup):
-    station = State()
-    bus = State()
-
 @bot.message_handler(commands = ['start'])
 async def say_hi(message):
     await bot.delete_state(message.from_user.id, message.chat.id)
+    await bot.send_chat_action(message.chat.id, 'typing')
     await bot.reply_to(message, f"Hello! I'm a bot that can help you get the latest bus timings!\nType /bus to get the bus timings for a certain stop!\nSend me your location to get info on all the bus stations closest to you! (We don't track your data!)")
 
 # The user sent us their location.
 @bot.message_handler(content_types = ['location'])
 async def get_nearest_bus_station_info(message):
+    await bot.send_chat_action(message.chat.id, 'typing')
     await bot.reply_to(message, lta_api_processor.display_nearest_bus_stations(message.location), parse_mode = "HTML")
 
 # The user is asking about a certain bus
@@ -68,63 +65,12 @@ async def retrieve_info(message):
     
     # Now we can trigger the process to get arrival timings.
     async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        await bot.send_chat_action(message.chat.id, 'typing')
         await bot.send_message(message.chat.id, lta_api_processor.display_arrivals(data['arrival_station'], bus), parse_mode = "HTML")
         await bot.delete_state(message.from_user.id, message.chat.id)
 
-@bot.message_handler(commands = ['nextbusalert'])
-async def remind_me_bus(message):
-    # Retrieves the arrival time of a bus, and sets a timer for a message to be sent to the user.
-    await bot.set_state(message.from_user.id, AlertCommandStates.station, message.chat.id)
-    await bot.send_message(message.chat.id, "Which bus stop do you want me to look out for?")
+async def bot_setup():
+    await asyncio.gather(lta_api_processor.query_static_data(),
+                            bot.infinity_polling())
 
-# TODO: Think of how to merge the duplicate functions below with the previous ones. For future, refactoring me. >:)
-@bot.message_handler(state = AlertCommandStates.station)
-async def alert_get_bus(message):
-    msg_txt = message.text
-    # A bus stop code is definitely 5 digits. 
-    # Unless the user is trying to mess with me.
-    try:
-        result = re.search(r"\d{5}", msg_txt)
-        station = msg_txt[result.start():result.end()]
-    except Exception as e:
-        print(str(e))
-        await bot.reply_to(message, "I couldn't find a valid bus station :<")
-        await bot.delete_state(message.from_user.id, message.chat.id)
-        return
-
-    await bot.set_state(message.from_user.id, AlertCommandStates.bus, message.chat.id)
-    # Now we ask them for the bus number, which should be some sequence of digits and maybe a letter at the back. 
-    # Unless the user is trying to mess with me.
-    await bot.send_message(message.chat.id, "Which bus service should I look out for?")
-    async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data['alert_station'] = station
-
-@bot.message_handler(state = AlertCommandStates.bus)
-async def set_alert(message):
-    msg_txt = message.text
-    try:
-        result = re.search(r"\d+[a-zA-Z]?", message.text)
-        bus = msg_txt[result.start():result.end()]
-    except Exception as e:
-        print(str(e))
-        await bot.reply_to(message, "I couldn't find a valid bus number :<")
-        return
-    
-    # Now we set an alarm for the next bus.
-    async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        station = data['alert_station']
-        bus_arrival_data = lta_api_processor.query_arrivals(station, bus)
-        bus_service = bus_arrival_data['Services'][0]
-        for arrival in [bus_service['NextBus'], bus_service['NextBus2'], bus_service['NextBus3']]:
-            if not arrival['OriginCode']: continue 
-            bus_est = lta_api_processor.bus_est_arrival_min(datetime.datetime.fromisoformat(arrival['EstimatedArrival']))
-            if bus_est > 2:
-                break
-
-    await bot.send_message(message.chat.id, f"I set an alarm for the next arrival of bus {bus} at station {station} :D\nYou will be notified 2 minutes before the bus is slated to arrive.")
-
-    await asyncio.sleep((bus_est - 2) * 60)
-    await bot.send_message(message.chat.id, f"Your bus {bus} is arriving at station {station} soon!")
-    await bot.delete_state(message.from_user.id, message.chat.id)
-
-asyncio.run(bot.infinity_polling())
+asyncio.run(bot_setup())
--- src/lta_api_processor.py
+++ src/lta_api_processor.py
@@ -3,72 +3,69 @@ import requests
 import datetime
 import telebot
 import json
+import asyncio
 from haversine import haversine, Unit
 
-# Dictionaries
-bus_route_dict = {
-    "bus_routes": {}
-}
 # The below functions process bus arrival timings and display them in a readable format.
+def request_bus_routes(skip: int) -> dict:
+    url = f"http://datamall2.mytransport.sg/ltaodataservice/BusRoutes?$skip={skip}"
+    r = requests.get(url, headers = headers)
+    return r.json()
 
-# TODO: Figure out a smarter way to determine is a bus is in operation or not.
-# def request_bus_routes(skip: int) -> dict:
-#     url = f"http://datamall2.mytransport.sg/ltaodataservice/BusRoutes?$skip={skip}"
-#     r = requests.get(url, headers = headers)
-#     return r.json()
+def get_all_bus_operation_times():
+    # Retrieves all the bus operation times by station from LTA.
+    
+    # We only ever run this function after get_all_bus_stations, therefore we can skip the try except clause.
+    bus_station_codes = [station["BusStopCode"] for station in load_from_storage_bus_stations()["bus_stops"]]
+    
+    bus_operation_dict = {
+        "operation_times" : {}
+    }
+    for station_code in bus_station_codes: bus_operation_dict["operation_times"][station_code] = {}
 
-# def get_all_bus_routes():
-#     # Retrieves all the bus services from LTA.
-#     bus_route_dict = {
-#         "bus_routes": {}
-#     }
-#     resp = {}
-#     query_count = 0
-#     while("value" not in resp.keys() or len(resp["value"])):
-#         resp = request_bus_routes(query_count * 500)
-#         for svc in resp["value"]: bus_route_dict["bus_routes"][svc["ServiceNo"]] = []
-#         for svc in resp["value"]: bus_route_dict["bus_routes"][svc["ServiceNo"]][svc["BusStopCode"]].append(svc)
-#         query_count += 1
+    resp = {}
+    query_count = 0
+    while("value" not in resp.keys() or len(resp["value"])):
+        resp = request_bus_routes(query_count * 500)
+        for svc in resp["value"]: 
+            bus_operation_dict["operation_times"][svc["BusStopCode"]][svc["ServiceNo"]] = svc
+        query_count += 1
 
-#     with open(f"{storage_path}bus_route_info.json", "w+") as f:
-#         bus_route_dict["last_updated"] = datetime.datetime.isoformat(datetime.datetime.now(tz = sg_timezone))
-#         f.write(json.dumps(bus_route_dict))
-#         print("Great Success!")
+    with open(f"{storage_path}bus_operation_info.json", "w+") as f:
+        bus_operation_dict["last_updated"] = datetime.datetime.isoformat(datetime.datetime.now(tz = sg_timezone))
+        f.write(json.dumps(bus_operation_dict))
 
-# def load_from_storage_bus_routes():
-#     with open(f"{storage_path}bus_route_info.json", "r") as f:
-#         return json.loads(f.read())
+def load_from_storage_bus_operation_times():
+    with open(f"{storage_path}bus_operation_info.json", "r") as f:
+        return json.loads(f.read())
 
-# # We split the week into weekdays, saturdays, and sundays. Because LTA does as well.
-# def is_weekday(weekday: int):
-#     # According to ISO 8601, 1 is a Monday, 2 is a Tuesday, and so on until 7, which is a Sunday.
-#     if weekday == 6: return 'SAT'
-#     if weekday == 7: return 'SUN'
-#     return 'WD'
+# We split the week into weekdays, saturdays, and sundays. Because LTA does as well.
+def is_weekday(weekday: int):
+    # According to ISO 8601, 1 is a Monday, 2 is a Tuesday, and so on until 7, which is a Sunday.
+    if weekday == 6: return 'SAT'
+    if weekday == 7: return 'SUN'
+    return 'WD'
 
-# def bus_in_operation(station: str, bus: str):
-#     global bus_route_dict
+def bus_in_operation(station: str, bus: str):
 
-#     # Checks if a bus is in operation.
-#     datetime_now = datetime.datetime.now(tz = sg_timezone)
-#     weekday = datetime_now.isoweekday()
-#     hhmm = datetime_now.strftime("%H%M")
-#     try:
-#         if not len(bus_route_dict["bus_routes"]): bus_route_dict = load_from_storage_bus_routes()
-#         # If it's been a day since we last updated this list, then retrieve the new list.
-#         if (datetime_now - datetime.datetime.fromisoformat(bus_route_dict["last_updated"])).days >= 1: 
-#             get_all_bus_routes()
-#             bus_route_dict = load_from_storage_bus_routes()
-#     except FileNotFoundError:
-#         get_all_bus_routes()
-#         if not len(bus_route_dict["bus_routes"]): bus_route_dict = load_from_storage_bus_routes()
-#     except Exception as e:
-#         print(str(e))
-#     finally:
-#         first_bus = bus_route_dict["bus_routes"][bus][station][f"{is_weekday(weekday)}_FirstBus"]
-#         last_bus = bus_route_dict["bus_routes"][bus][station][f"{is_weekday(weekday)}_LastBus"]
-
-#         return not(int(last_bus) < int(hhmm) < int(first_bus))
+    # Checks if a bus is in operation.
+    datetime_now = datetime.datetime.now(tz = sg_timezone)
+    weekday = datetime_now.isoweekday()
+    hhmm = int(datetime_now.strftime("%H%M"))
+    try:
+        bus_operation_dict = load_from_storage_bus_operation_times()
+    except FileNotFoundError:
+        get_all_bus_stations()
+        get_all_bus_routes()
+        bus_operation_dict = load_from_storage_bus_operation_times()
+    except Exception as e:
+        print(str(e))
+    finally:
+        arrival_data = bus_operation_dict["operation_times"][station][bus]
+        first_bus = int(arrival_data[f"{is_weekday(weekday)}_FirstBus"])
+        last_bus = int(arrival_data[f"{is_weekday(weekday)}_LastBus"])
+        if last_bus < first_bus: last_bus += 2400
+        return first_bus < hhmm and hhmm < last_bus
 
 def bus_est_arrival_min(time1: datetime.datetime) -> int:
     return round((time1 - datetime.datetime.now(tz = sg_timezone)).seconds / 60)
@@ -78,31 +75,37 @@ def get_bus_arrival_status(time1: datetime.datetime) -> str:
     # This is probably because:
     # - The bus actually left
     # - L + ratio + infrequent updates + bad bus timing
-    # In order to preserve my social credits, I'm assuming the former.
-    if time1 < datetime.datetime.now(tz = sg_timezone):
-        return "Gone"
-    
+    # Due to negative experiences with bus timings I am now assuming the latter.    
     est = bus_est_arrival_min(time1)
 
-    if not(est): return "Arriving Soon"
+    if est <= 0: return "Arriving Soon"
     
     return f"{str(est)} min"
 
 def bus_ordering(element):
     # This ordering ensures that services and their letter variants (e.g. 89 and 89e) will be displayed next to each other.
-    return int("".join([i for i in element['ServiceNo'] if i.isdigit()]))
+    return int("".join([i for i in element if i.isdigit()]))
 
-def parse_arrival_data(bus_arrival_data: dict):
+def parse_arrival_data(bus_arrival_data: dict, bus_operation_data: dict, bus: str):
     result = ""
-    if not('Services' in bus_arrival_data or len(bus_arrival_data['Services'])):
+    if bus != "-1" and not(bus in bus_operation_data):
         return "I can't find this bus service. Please check that you have not entered the wrong bus station code."
 
-    for service in sorted(bus_arrival_data['Services'], key = bus_ordering):
-        result += f"<b>Bus No. {service['ServiceNo']}</b>\n"
-        # if 'NotOperational' in service:
-        #     result += "Not Operating"
-        #     continue
+    services = bus_arrival_data['Services']
+    print(sorted(bus_operation_data.keys(), key = bus_ordering))
+    for service_no in sorted(bus_operation_data.keys(), key = bus_ordering):
+        service = {}
+        for i in services:
+            if i['ServiceNo'] == service_no: 
+                service = i
+                break
         
+        result += f"<b>Bus No. {service_no}</b>\n"
+        
+        if not bus_in_operation(bus_arrival_data['BusStopCode'], service_no) or service == {}:
+            result += "This bus is not operational at this moment.\n\n"
+            continue
+
         for bus in [service['NextBus'], service['NextBus2'], service['NextBus3']]:
             if not bus['EstimatedArrival']: continue 
             arrival_status = get_bus_arrival_status(datetime.datetime.fromisoformat(bus['EstimatedArrival']))
@@ -138,11 +141,11 @@ def load_from_storage_arrival_info(station: str) -> dict:
 def query_arrivals(station: str, bus: str) -> dict:
     # Lazily retrieve the arrival information for each service.
     bus_arrival_data = {}
+    bus_operation_data = {}
     try:
         bus_arrival_data = load_from_storage_arrival_info(station)
-        
-        # If it's been about 15 seconds since we last updated this list, then retrieve the new list.
-        if (datetime.datetime.now(tz = sg_timezone) - datetime.datetime.fromisoformat(bus_arrival_data["last_updated"])).seconds >= 15: 
+        # If it's been about 10 seconds since we last updated this list, then retrieve the new list.
+        if (datetime.datetime.now(tz = sg_timezone) - datetime.datetime.fromisoformat(bus_arrival_data["last_updated"])).seconds >= 10: 
             get_arrivals(station, bus)
             bus_arrival_data = load_from_storage_arrival_info(station)
     except FileNotFoundError:
@@ -155,12 +158,12 @@ def query_arrivals(station: str, bus: str) -> dict:
             if bus_arrival_data['Services'][i]['ServiceNo'] != bus:
                 bus_arrival_data['Services'].pop(i)
 
-
         return bus_arrival_data
 
 def display_arrivals(station: str, bus: str) -> str:
     bus_arrival_data = query_arrivals(station, bus)
-    return parse_arrival_data(bus_arrival_data)
+    bus_operation_data = load_from_storage_bus_operation_times()["operation_times"][station]
+    return parse_arrival_data(bus_arrival_data, bus_operation_data, bus)
 
 def request_bus_station_data(skip: int = 0) -> dict:
     url = f"http://datamall2.mytransport.sg/ltaodataservice/BusStops?$skip={skip}"
@@ -204,12 +207,9 @@ def query_nearest_bus_stations(location: telebot.types.Location) -> dict:
     bus_station_dict = {}
     try:
         bus_station_dict = load_from_storage_bus_stations()
-        # If it's been a day since we last updated this list, then retrieve the new list.
-        if (datetime.datetime.now(tz = sg_timezone) - datetime.datetime.fromisoformat(bus_station_dict["last_updated"])).days >= 1: 
-            get_all_bus_stations()
-        bus_station_dict = load_from_storage_bus_stations()
     except FileNotFoundError as e:
         get_all_bus_stations()
+        get_all_bus_routes()
         bus_station_dict = load_from_storage_bus_stations()
     finally:
         closest_neighbors = get_closest_bus_stations(location, bus_station_dict["bus_stops"])
@@ -222,3 +222,17 @@ def display_nearest_bus_stations(location: telebot.types.Location):
         result = result + f"{neighbor_name} Station ({round(dist)}m away):\n{display_arrivals(neighbor_code, '-1')}{'=' * 20}\n\n"
     
     return result
+
+async def query_static_data():
+    # In this function, we lazily query the "static" data inherent to the public bus system in SG.
+    # As one can expect, this includes information like bus stops and bus operation timings.
+    while True:
+        # TODO: Devise a way to integrate both get_all_bus_stations and get_all_bus_operation_times without too much clutter.
+        print("Refreshing bus network information...")
+        get_all_bus_stations()
+        get_all_bus_operation_times()
+        print("Done!")
+        
+        tmwdatetime = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days = 1), datetime.time())
+        wait_sec = datetime.timedelta(seconds = (tmwdatetime - datetime.datetime.now()).seconds, microseconds = (tmwdatetime - datetime.datetime.now()).microseconds)
+        await asyncio.sleep(wait_sec.seconds + wait_sec.microseconds / 1000000)
